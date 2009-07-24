e1d0e2e418fb93fe9408563bc9031feaf16ccc70
Your Name <nik.wolfgramm@gmail.com>
2009-07-25 00:43:02 +0200

    added options for mysql paths

--- lib/mysqlmb.rb
+++ lib/mysqlmb.rb
@@ -11,12 +11,12 @@ class MySQLMaint
     @password = connection[:password]
     @host = connection[:host]
     @credentials = "--user=#{@user} --password=#{@password}"
-    @backup_path = rm_slash(paths[:backup])
-    @mysql_path = rm_slash(paths[:mysql])
     @date_format = params[:date_format] || "%Y-%m-%d"
     @verbose = params[:verbose] || false 
     logfile = File.open(paths[:logfile], File::WRONLY | File::APPEND | File::CREAT)
     @logger = Logger.new(logfile, 10, 1024000)
+    @paths = {}
+    paths.each { |key, value| @paths[key] = rm_slash(value) }
   end
 
   def db_backup(databases=[])
@@ -28,7 +28,7 @@ class MySQLMaint
     databases.each do |db|
       dump_file = "#{@backup_path}/#{back_date()}-#{db}"
       @logger.add(Logger::INFO, "Backing up database #{db} ...")
-      %x[#{@mysql_path}/mysqldump --opt --flush-logs --allow-keywords -q -a -c #{@credentials} --host=#{@host} #{db} > #{dump_file}.tmp]
+      %x[#{@paths[:mysqldump]} --opt --flush-logs --allow-keywords -q -a -c #{@credentials} --host=#{@host} #{db} > #{dump_file}.tmp]
       if $? == 0
         %x[mv #{dump_file}.tmp #{dump_file}; bzip2 -f #{dump_file}]
         message = "INFO: Successfully backed up database: #{db}"
@@ -58,7 +58,7 @@ class MySQLMaint
     databases.each do |db|
      # make sure the database exists
      unless all_dbs.include?(db)
-       %x[echo CREATE DATABASE \\`#{db}\\` | mysql #{@credentials}]
+       %x[echo CREATE DATABASE \\`#{db}\\` | #{@paths[:mysql]}  #{@credentials}]
        if $? != 0
          error_count += 1
          message = "ERROR: can't create database #{db} message: #{$?}"
@@ -77,7 +77,7 @@ class MySQLMaint
      # decompress dump file
      %x[bunzip2 -k #{dump_file}.bz2]
      # restore database
-     %x[mysql #{@credentials} --host=#{@host} #{db} < #{dump_file}]
+     %x[#{@paths[:mysql]} #{@credentials} --host=#{@host} #{db} < #{dump_file}]
      if $? != 0
        error_count += 1
        message = "ERROR: can't restore database #{db} message: #{$?}"
@@ -107,7 +107,7 @@ class MySQLMaint
   end
 
   def chkdb()
-    check = %x[#{@mysql_path}/mysqlcheck --optimize -A #{@credentials} --host=#{@host}]
+    check = %x[#{@paths[:mysqlcheck]} --optimize -A #{@credentials} --host=#{@host}]
     @logger.add(Logger::INFO, check)
     puts check if @verbose
   end
@@ -139,7 +139,7 @@ class MySQLMaint
   def all_databases(source = "mysql", time = 0)
     case source
     when "mysql"
-      databases = %x[echo "show databases" | mysql #{@credentials} | grep -v Database].split("\n")
+      databases = %x[echo "show databases" | #{@paths[:mysql]} #{@credentials} | grep -v Database].split("\n")
       if $? != 0
         @logger.add(Logger::ERROR, "mysql \"show databases\" failed: return value #{$?}, user: #{@user}, using password: #{!@password.empty?}")
         exit
--- lib/parser.rb
+++ lib/parser.rb
@@ -18,14 +18,14 @@ module MySqlMb
  
     def initialize
       @connection = {}
-      @paths = {:logfile => APP_PATH + "/log/mysqlmb.log"}
+      @paths = {}
       @options = {}
-      @databases = []
     end
   
     def parse(command, args)
       optp = optparse(command, args)
       load_configfile(@options[:config_file]) if @options[:config_file]
+      set_defaults()
       list_options if @options[:debug]
       verify_input(optp, command)
       return @connection, @paths, @options, @databases
@@ -76,6 +76,33 @@ module MySqlMb
          end
        end
     end
+    
+    def set_defaults
+      # default databases (all)
+      @databases = []
+      
+      # default connetion
+      @connection[:user]        ||= 'backup'
+      @connection[:password]    ||= ''
+      
+      # default paths
+      @paths[:logfile]          ||= (APP_PATH + "/log/mysqlmb.log")
+      @paths[:backup]           ||=  File.expand_path(APP_PATH, '/backups')
+      @paths[:mysql]            ||= '/usr/bin/mysql'
+      @paths[:mysqldump]        ||= '/usr/bin/mysqldump'
+      @paths[:mysqlcheck]       ||= '/usr/bin/mysqlcheck'
+      
+      # default options
+      @options[:optimize]       ||= true
+      @options[:retention]      ||= 30
+      @options[:restore_offset] ||= 1
+      @options[:mail_to]        ||= ''
+      @options[:mail]           ||= false
+      @options[:list_type]      ||= :mysql
+      @options[:date_format]    ||= "%Y-%m-%d"
+      @options[:debug]          ||= false
+      @options[:verbose] = false
+    end
  
     def missing_credentials?(command)
       if (@connection[:user] == '' || @connection[:password] == '')
@@ -108,69 +135,56 @@ module MySqlMb
       opts.separator "Options:"
  
       # Define the options, and what they do
-      @connection[:user] ||= 'backup'
       opts.on( '-u', '--user USER', 'MySQL backup user (default: backup)' ) do |user|
         @connection[:user] = user
       end
  
-      @connection[:password] ||= ''
       opts.on( '-p', '--password PASSWORD', 'MySQL password' ) do |password|
         @connection[:password] = password
       end
       
-      @databases ||= []
       opts.on( '-d', '--databases db1,db2,db3', Array, 'Define which databases to backup (default: all)',
                                                        'Syntax: database1,database2',
                                                        'Keywords: all, user, system') do |db|
         @databases = db
       end
  
-      @options[:optimize] ||= true
       opts.on( '--[no-]optimize', 'Disable database optimization' ) do |o|
         @options[:optimize] = o
       end
  
-      @options[:retention] ||= 30
       opts.on( '-r', '--retention-time DAYS', Integer, 'How many days to keep the db backups (default: 30 days)' ) do |days|
         @options[:retention] = days
       end
  
-      @options[:restore_offset] = 1
       opts.on( '-t', '--time-offset DAYS', Integer, 'How old are the backups to restore (default: 1 (yesterday))' ) do |days|
         @options[:restore_offset] = days
       end
  
-      @options[:mail_to] ||= ''
       opts.on( '-m', '--mail-to MAIL-ADDRESS', 'email address to send reports to' ) do |mail|
         @options[:mail_to] = mail
       end
  
-      @options[:mail] ||= 'false'
       opts.on( '--[no-]mail', 'activate/deactivate mail messages' ) do |mail|
         @options[:mail] = mail
       end
  
-      @options[:list_type] ||= :mysql
       opts.on( '-l', '--list-type TYPE', [:mysql, :backup], 'Select list type (mysql, backup)' ) do |type|
         @options[:list_type] = type
       end
  
-      @paths[:backup] ||=  File.expand_path(APP_PATH, '/backups')
       opts.on( '--backup-path PATH', "backup storage directory (default: #{@paths[:backup]})" ) do |backup_path|
         @paths[:backup] = backup_path
       end
  
-      @paths[:mysql] ||= '/usr/bin/'
       opts.on( '--mysql-path PATH', 'Specify the MySQL utility path (default: /usr/bin/)' ) do |mysql_path|
         @paths[:mysql] = mysql_path
       end
  
-      @options[:date_format] ||= "%Y-%m-%d"
       opts.on( '--date-format FORMAT', 'Date format for backup file name (default: %Y-%m-%d)' ) do |format|
         @options[:date_format] = format
       end
       
-      @options[:config_file] = nil
       opts.on( '-c', '--config-file FILE', 'Specify a configuration file which contains all options',                                          'see config/config.rb.orig for an example' ) do |file|
         unless File.exists?(file)
           puts "Abort: No configuration file found!\nSee #{APP_PATH}/config/mysqlmb.conf.dist for an example."
@@ -179,12 +193,10 @@ module MySqlMb
         @options[:config_file] = file
       end
       
-      @options[:verbose] = false
-        opts.on( '--verbose', 'Output more information' ) do
+      opts.on( '--verbose', 'Output more information' ) do
         @options[:verbose] = true
       end
  
-      @options[:debug] = false
       opts.on( '--debug', 'Debugging mode: show arguments passed' ) do
         @options[:verbose] = true
         @options[:debug] = true
