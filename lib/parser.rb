e1ed7b7458d732d4bea12189f8a8e0a131677617
niwo <nik.wolfgramm@gmail.com>
2009-07-24 17:34:25 +0200

    fixed config file handling, improved debug output

--- lib/parser.rb
+++ lib/parser.rb
@@ -14,7 +14,7 @@ module MySqlMb
     CMD = %w[backup restore optimize list]
 
     # commands which require no MySQL user/password
-    NO_CREDENTIALS = {'list' => {:list_type => :backup}}
+    NO_CREDENTIALS = {'list' => {:list_type => :backup}
 
     def initialize
       @connection = {}
@@ -24,16 +24,42 @@ module MySqlMb
     end
   
     def parse(command, args)
-      optparse(command, args)
+      optp = optparse(command, args)
+      load_configfile(@options[:config_file]) if @options[:config_file]
       list_options if @options[:debug]
+      verify_input(optp, command)
       return @connection, @paths, @options, @databases
     end
     
     private
 
     def list_options
-      @options.each {|key, value| puts "#{key}: #{value.to_s || 'nil'}" }
+      puts "Connection:"
+      @connection.each do |key, value|
+        value.gsub!(/./) { |s| "*" } if key == :password
+        puts "\t#{key}: #{value.to_s || 'nil'}"
+      end
+      puts "Paths:"
+      @paths.each {|key, value| puts "\t#{key}: #{value.to_s || 'nil'}" }
+      puts "Options:"
+      @options.each {|key, value| puts "\t#{key}: #{value.to_s || 'nil'}" }
       exit
+    end
+
+    def verify_input(optp, command)
+      if missing_credentials? command
+        puts "Please provide at least a password for MySQL user \"#{@connection[:user]}\""
+        puts
+        puts optp.help
+        return false 
+      end
+
+      unless @options[:version] || CMD.include?(command)
+        puts "Please provide a valid action argument:"
+        puts
+        puts optp.help
+        return false
+      end
     end
 
     def load_configfile(file)
@@ -43,7 +69,7 @@ module MySqlMb
          if [:host, :user, :password].include? key
            @connection[key] = value
          # path values
-         elsif [:backup, :mysql, :mysqldump]
+         elsif [:backup, :mysql, :mysqldump].include? key
            @paths[key] = value
          else
            @options[key] = value
@@ -110,7 +136,7 @@ module MySqlMb
       end
 
       @options[:restore_offset] = 1
-      opts.on( '-o', '--restore-offset DAYS', Integer, 'How old are the backups to restore (default: 1 day)' ) do |days|
+      opts.on( '-t', '--time-offset DAYS', Integer, 'How old are the backups to restore (default: 1 (yesterday))' ) do |days|
         @options[:restore_offset] = days
       end
 
@@ -144,17 +170,17 @@ module MySqlMb
         @options[:date_format] = format
       end
       
-      @config_file = nil
+      @options[:config_file] = nil
       opts.on( '-c', '--config-file FILE', 'Specify a configuration file which contains all options',                                          'see config/config.rb.orig for an example' ) do |file|
-        @config_file = file
         unless File.exists?(file)
           puts "Abort: No configuration file found!\nSee #{APP_PATH}/config/mysqlmb.conf.dist for an example."
           exit
         end
+        @options[:config_file] = file
       end
       
       @options[:verbose] = false
-        opts.on( '-V', '--verbose', 'Output more information' ) do
+        opts.on( '--verbose', 'Output more information' ) do
         @options[:verbose] = true
       end
 
@@ -171,32 +197,20 @@ module MySqlMb
         return false
       end
 
-      opts.on_tail('-v', '--version', "Show version") do
+      opts.on_tail( '--version', "Show version" ) do
+          @options[:lala] = 'lulu'
           puts "#{opts.program_name} v.#{opts.version}, written by Nik Wolfgramm"
           puts
           puts "Copyright (C) 2009 Nik Wolfgramm"
           puts "This is free software; see the source for copying conditions."
           puts "There is NO warranty; not even for MERCHANTABILITY or"
           puts "FITNESS FOR A PARTICULAR PURPOSE."
-          abort
+          exit
         end
       end
       
       optparse.parse!(args)
-      
-      if missing_credentials? command
-        puts "Please provide at least a password for MySQL user \"#{@connection[:user]}\""
-        puts
-        puts optparse.help
-        return false 
-      end
-
-      unless @options[:version] || CMD.include?(command)
-        puts "Please provide a valid action argument:"
-        puts
-        puts optparse.help
-        return false
-      end
+      return optparse  
     end
 
   end # class
